1b8b280057bcea812913dd0fb98212de35aed3f6
CSimbulan <c.simbulan@hotmail.com>
2019-05-10 11:42:18 -0400

    changed tertiary sort for iv to hp20

--- src/App.js
+++ src/App.js
@@ -195,7 +195,7 @@ class App extends Component {
     var index3 = 0; /*Tertiary sort index.*/
     if (this.state.options.sort === "iv") {
       index2 = 0; /*If primary sort is by IV, secondary sort is by CP@20.*/
-      index3 = 9; /*If primary sort is by IV, tertiary sort is by HP@40.*/
+      index3 = 1; /*If primary sort is by IV, tertiary sort is by HP@20.*/
     } else {
       index2 = 8; /*If primary sort is by CP@20, secondary sort is by CP@40.*/
       index3 = 4; /*If primary sort is by IV, teriary sort is by IV.*/
